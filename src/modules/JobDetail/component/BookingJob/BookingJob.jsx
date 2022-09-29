de79763d3892e7b65aa5abf575bca316450552bc
truc2203 <tranthanhtruc22031998@gmail.com>
2022-09-29 21:06:11 +0700

    booking job

---
+++ src/modules/JobDetail/component/BookingJob/BookingJob.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import { useState } from "react";
+import {BsFillClockFill} from 'react-icons/bs'
+import {BiRevision} from 'react-icons/bi'
+import {FcCheckmark,FcDebian} from 'react-icons/fc'
+const BookingJob = () => {
+  const [side1, setSide1] = useState("none");
+  const [side2, setSide2] = useState("none");
+  const [side3, setSide3] = useState("none");
+
+  const handleSide = (value) => {
+    if (value === "side-basic") {
+      setSide1("block");
+      setSide2("none");
+      setSide3("none");
+    } else if (value === "side-standard") {
+      setSide2("block");
+      setSide1("none");
+      setSide3("none");
+    } else if (value === "side-premium") {
+      setSide3("block");
+      setSide2("none");
+      setSide1("none");
+    }
+  };
+
+  return (
+    <div>
+      <div className="d-flex sidebar-hd">
+        <button
+          onClick={() => handleSide("side-basic")}
+          className="sidebar-btn col-4 border"
+        >
+          Basic
+        </button>
+        <button
+          onClick={() => handleSide("side-standard")}
+          className="sidebar-btn col-4 border"
+        >
+          Standard
+        </button>
+        <button
+          onClick={() => handleSide("side-premium")}
+          className="sidebar-btn col-4 border"
+        >
+          Premium
+        </button>
+      </div>
+      <div className="sidebar-content position-relative ">
+        <div
+          style={{ display: `${side1}` }}
+          className="side-item p-4 position-absolute border"
+        >
+            <div className="d-flex flex-column pb-4">
+                <p className="side-price">$100</p>
+                <p className="side-discount">Save up to 15% with Subscribe to Save</p>
+            </div>
+            <div className="side-discount pb-5">
+            BASIC Basic Static Native Android and iPhone mobile app development for 4 - 5 screens
+            </div>
+            <div className="d-flex pb-3">
+                <div className="d-flex align-items-center side-discount pe-3">
+                    <p className="pe-2"><BsFillClockFill/></p>
+                    <p>5 Days Delivery</p>
+                </div>
+                <div className="d-flex align-items-center side-discount">
+                    <p className="pe-2"><BiRevision/></p>
+                    <p>3 Revisions</p>
+                </div>
+            </div>
+            <ul className="d-flex flex-column side-feature pb-3">
+                    <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Functional app</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>2 operating systems</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App submission</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App icon</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Splash screen</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcDebian/></p>
+                        <p>Ad network integration</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcDebian/></p>
+                        <p>Source code</p>
+                    </li>
+                </ul>
+                <button className="side-btn">Continue</button>
+                <button className="side-btn-sub">Compare Packages</button>
+        </div>
+        <div
+          style={{ display: `${side2}` }}
+          className="side-item p-4 position-absolute border"
+        >
+             <div className="d-flex flex-column pb-4">
+                <p className="side-price">$500</p>
+                <p className="side-discount">Save up to 15% with Subscribe to Save</p>
+            </div>
+            <div className="side-discount pb-5">
+            STANDARD Average level a server based android and iPhone app with 4-5 screens with your given UI
+            </div>
+            <div className="d-flex pb-3">
+                <div className="d-flex align-items-center side-discount pe-3">
+                    <p className="pe-2"><BsFillClockFill/></p>
+                    <p>10 Days Delivery</p>
+                </div>
+                <div className="d-flex align-items-center side-discount">
+                    <p className="pe-2"><BiRevision/></p>
+                    <p>5 Revisions</p>
+                </div>
+            </div>
+            <ul className="d-flex flex-column side-feature pb-3">
+            <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Functional app</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>2 operating systems</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App submission</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App icon</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Splash screen</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcDebian/></p>
+                        <p>Ad network integration</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Source code</p>
+                    </li>
+                </ul>
+                <button className="side-btn">Continue</button>
+                <button className="side-btn-sub">Compare Packages</button>
+        </div>
+        <div
+          style={{ display: `${side3}` }}
+          className="side-item p-4 position-absolute border"
+        >
+             <div className="d-flex flex-column pb-4">
+                <p className="side-price">$900</p>
+                <p className="side-discount">Save up to 15% with Subscribe to Save</p>
+            </div>
+            <div className="side-discount pb-5">
+            PREMIUM A complete product developed as per requirements using Database, Payment SDKs
+            </div>
+            <div className="d-flex pb-3">
+                <div className="d-flex align-items-center side-discount pe-3">
+                    <p className="pe-2"><BsFillClockFill/></p>
+                    <p>21 Days Delivery</p>
+                </div>
+                <div className="d-flex align-items-center side-discount">
+                    <p className="pe-2"><BiRevision/></p>
+                    <p>Unlimited Revisions</p>
+                </div>
+            </div>
+            <ul className="d-flex flex-column side-feature pb-3">
+            <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Functional app</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>2 operating systems</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App submission</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>App icon</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Splash screen</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Ad network integration</p>
+                    </li> <li className="d-flex pb-2">
+                        <p className='pe-2'><FcCheckmark/></p>
+                        <p>Source code</p>
+                    </li>
+                </ul>
+                <button className="side-btn">Continue</button>
+                <button className="side-btn-sub">Compare Packages</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingJob;
--- src/modules/JobDetail/pages/JobDetail.jsx
+++ src/modules/JobDetail/pages/JobDetail.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import BookingJob from '../component/BookingJob/BookingJob'
 import Comments from '../component/Comments'
 import JD from '../component/JD'
+
 const JobDetail = () => {
 
   const data = useParams()
@@ -9,13 +11,13 @@ const JobDetail = () => {
   return (
     <div className='m-container'>
       <div className="p-4">
-      <div className="d-flex align-items-center">
+      <div className="d-flex ">
         <div className="col-8 jd-page">
             <JD id={id}/>
             <Comments id={id}/>
         </div>
         <div className="col-4">
-
+            <BookingJob/>
         </div>
       </div>
     </div>
