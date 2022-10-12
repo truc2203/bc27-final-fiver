8b5941b4eb8dbb1dafac23b0a5dbb3c5260c06bd
Nguyen Tuan <nguyenngoctuan2500@gmail.com>
2022-10-12 12:25:06 +0700

    user

--- src/apis/userAPI.js
+++ src/apis/userAPI.js
@@ -1,19 +1,19 @@
 import axiosClient from "./axiosClient";
 const userAPI = {
-    getUser : (userId) => {
-      return axiosClient.get(`users/${userId}`)
-    },
-    editUser : (value,userId) => {
-      return axiosClient.put(`users/${userId}`,value)
-    },
-    getBooked : () => {
-      return axiosClient.get(`thue-cong-viec`)
-    },
-    getBookedByUser : (id) => {
-      return axiosClient.get(`thue-cong-viec/{id}`)
-    },
-    getBookedById : () => {
-      return axiosClient.get(`thue-cong-viec/lay-danh-sach-da-thue`)
-    }
+  getUser: () => {
+    return axiosClient.get("users");
+  },
+  editUser: (value, userId) => {
+    return axiosClient.put(`users/${userId}`, value);
+  },
+  getBooked: () => {
+    return axiosClient.get(`thue-cong-viec`);
+  },
+  getBookedByUser: (id) => {
+    return axiosClient.get(`thue-cong-viec/{id}`);
+  },
+  getBookedById: () => {
+    return axiosClient.get(`thue-cong-viec/lay-danh-sach-da-thue`);
+  },
 };
 export default userAPI;
--- src/modules/UserAdmin/User.jsx
+++ src/modules/UserAdmin/User.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
-
-const User = () => {
-  return (
-    <div>User</div>
-  )
+import React, { useState } from "react";
+import useRequest from "../../hook/useRequest";
+import userAPI from "../../../src/apis/userAPI";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { UserOutlined } from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, notification } from "antd";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+const User = () => {
+  const [value, setValue] = useState(null);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useRequest(() => userAPI.getUser(value ? value : null), {
+    deps: [value],
+  });
+  const navigate = useNavigate();
+  const movePath = (path) => {
+    navigate(path);
+  };
+  console.log(users);
+  const dispatch = useDispatch();
+  const [collapsed, setCollapsed] = useState(false);
+  const handleDeleteUser = () => {};
+  const handleEditUser = (userr) => {};
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <h4 className="text-dark mb-3">Quản lý người dùng</h4>
+            <button
+              onClick={() => movePath("add")}
+              className="btn-style mb-3"
+              style={{ width: "250px" }}
+            >
+              Thêm người dùng
+            </button>
+            <form>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="Nhập tài khoản"
+                  style={{ display: "inline-block", width: "80%" }}
+                  className="form-control"
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </div>
+            </form>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <table className="table">
+              <thead>
+                <th>Tài Khoản</th>
+                <td>birthday</td>
 
-export default User
+                <th>Email</th>
+                <th>skills</th>
+                <th>Hành động</th>
+              </thead>
+              {users?.map((userr) => {
+                return (
+                  <tbody key={userr.id}>
+                    <tr>
+                      <td data-label="name">{userr.name}</td>
+                      <td>{userr.birthday}</td>
+                      <td>{userr.email}</td>
+                      <td>{userr.skill}</td>
+                      <td>
+                        {" "}
+                        <button
+                          onClick={() => handleDeleteUser(userr.id)}
+                          className="px-2 fs-5"
+                        >
+                          <AiOutlineDelete />
+                        </button>
+                        <button
+                          onClick={() => handleEditUser(userr)}
+                          className="fs-5"
+                        >
+                          <AiOutlineEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default User;
